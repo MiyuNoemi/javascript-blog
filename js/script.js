87ed970489913d7f0548e958f073c5ebb1f0c95b
WeronikaBrzóska <weronikabrzoska97@gmail.com>
2020-01-17 11:53:02 +0100

    remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@
 //     const links = document.querySelectorAll('.titles a');
 //     console.log('links:', links);
 //   });
+//"use strict";
 
 const titleClickHandler = function(event){
     console.log('Link was clicked!', event);
   
     /* remove class 'active' from all article links  */
-  
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
@@ -24,3 +37,5 @@ const titleClickHandler = function(event){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+ 
